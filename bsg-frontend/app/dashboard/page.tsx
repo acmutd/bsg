1a1f3266b4bbb4cb47b292d05b9ed7c5eadbc18f
Yash Shah <62358049+git-yash@users.noreply.github.com>
2024-02-14 11:25:01 -0600

    refactored difficulty component

--- bsg-frontend/app/dashboard/page.tsx
+++ bsg-frontend/app/dashboard/page.tsx
@@ -1,44 +1,17 @@
 "use client"
 import React from "react";
 import SearchBar from "@/components/customComponents/searchbar/searchbar";
-import {
-    DropdownMenu,
-    DropdownMenuContent,
-    DropdownMenuRadioGroup,
-    DropdownMenuRadioItem,
-    DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
-import {Button} from "@/components/ui/button"
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faAngleDown} from "@fortawesome/free-solid-svg-icons";
+import DifficultyDropdown from "@/components/customComponents/difficultyDropdown/difficultyDropdown";
+import Difficulty from "@/app/models/Difficulty";
 
 const Dashboard = () => {
-    const [position, setPosition] = React.useState("Easy")
+    const [position, setPosition] = React.useState<Difficulty>(Difficulty.Easy);
 
     return (
         <div>
             <div className={'justify-center space-y-3'}>
                 <SearchBar/>
-                <DropdownMenu>
-                    <DropdownMenuTrigger asChild>
-                        <div className={'flex-row'}>
-                            <Button
-                                className={'space-x-2 bg-inputBackground hover:brightness-125 hover:bg-inputBackground'}>
-                                <p>Difficulty</p>
-                                <FontAwesomeIcon icon={faAngleDown}/>
-                            </Button>
-                        </div>
-                    </DropdownMenuTrigger>
-                    <DropdownMenuContent className="w-56 bg-inputBackground">
-                        <DropdownMenuRadioGroup value={position} onValueChange={setPosition}>
-                            <DropdownMenuRadioItem value='Easy'
-                                                   className={'text-green-400'}>Easy</DropdownMenuRadioItem>
-                            <DropdownMenuRadioItem value="Medium"
-                                                   className={'text-yellow-400'}>Medium</DropdownMenuRadioItem>
-                            <DropdownMenuRadioItem value="Hard" className={'text-red-400'}>Hard</DropdownMenuRadioItem>
-                        </DropdownMenuRadioGroup>
-                    </DropdownMenuContent>
-                </DropdownMenu>
+                <DifficultyDropdown position={position} setPosition={setPosition}/>
             </div>
         </div>
     );
