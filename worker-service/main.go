814dc63175bcf89de0148b22f7e52b205013539d
minhhuy24072002 <minhhuy24072002@gmail.com>
2024-06-05 17:27:47 -0500

    refactor main.go to match with new kafka_util

--- worker-service/main.go
+++ worker-service/main.go
@@ -4,28 +4,19 @@ import (
 	"encoding/json"
 	"fmt"
 	"os"
-	"strconv"
-	"strings"
 
 	kafka_dto "github.com/acmutd/bsg/worker-service/kafka/kafka-dto"
 	kafka_queue "github.com/acmutd/bsg/worker-service/kafka/kafka-queue"
-	"github.com/acmutd/bsg/worker-service/leetcode-worker/lib"
 	"github.com/confluentinc/confluent-kafka-go/kafka"
 )
 
 var (
-	leetcode_username        string = os.Getenv("LEETCODE_USERNAME")
-	leetcode_password        string = os.Getenv("LEETCODE_PASSWORD")
-	drivers_path             string = "./leetcode-worker/drivers/chromedriver"
 	kafka_server             string = os.Getenv("KAFKA_BROKER")
 	submission_request_topic string = os.Getenv("KAFKA_INGRESS_TOPIC")
 	submission_verdict_topic string = os.Getenv("KAFKA_EGRESS_TOPIC")
 )
 
 func main() {
-	// Login into LeetCode
-	LEETCODE_SESSION, CSRF_Token := lib.Login(leetcode_username, leetcode_password, drivers_path)
-
 	// Create new kafka consumer
 	consumer := kafka_queue.NewKafkaConsumer(kafka_server, submission_request_topic, "worker")
 	if consumer == nil {
@@ -48,36 +39,19 @@ func main() {
 		}
 		go func() {
 			// Process submission request
-			problem_slug, problem_id, lang, code := parseSubmissionRequest(request)
-			submission_id, verdict, err := processSubmissionRequest(LEETCODE_SESSION, CSRF_Token, problem_slug, problem_id, lang, code)
+			submissionResult, err := processSubmissionRequest(request)
 			if err != nil {
 				fmt.Println("Failed to process request due to error - ", err.Error())
 				return
 			}
-
-			// Process submission id
-			submissionID, err := strconv.ParseUint(submission_id, 10, 32)
-			if err != nil {
-				fmt.Println("Failed to parse submission id due to error - ", err.Error())
-				return
-			}
-
-			// Serialize submission id and verdict into a KafkaEgressDTO struct
-			egressDTO := kafka_dto.KafkaEgressDTO{
-				SubmissionId: uint(submissionID),
-				Verdict:      verdict,
-			}
-
 			// JSON Marshal the submission result
-			marshalData, marshalErr := json.Marshal(egressDTO)
+			submissionResultBytes, marshalErr := json.Marshal(submissionResult)
 			if marshalErr != nil {
 				fmt.Println("Unable to marshal submission result due to " + marshalErr.Error())
 				return
 			}
-
 			// Send submission verdict
-			partitionAny := int(kafka.PartitionAny)
-			err = kafka_queue.SendSubmissionResult(producer, kafka_server, submission_verdict_topic, string(marshalData), partitionAny)
+			err = kafka_queue.SendEvent(producer, kafka_server, submission_verdict_topic, submissionResultBytes)
 			if err != nil {
 				fmt.Println("Failed to send submission verdict due to error - ", err.Error())
 				return
@@ -86,38 +60,31 @@ func main() {
 	}
 }
 
-// TODO(minhhuy24072002): Implement the parseSubmissionRequest function
-func parseSubmissionRequest(request string) (string, int, string, string) {
-	return "", -1, "", ""
-}
-
-func receiveSubmissionRequest(consumer *kafka.Consumer) (string, error) {
+func receiveSubmissionRequest(consumer *kafka.Consumer) (kafka_dto.KafkaIngressDTO, error) {
 	// Create a channel for receiving submission request
-	request := make(chan string)
+	request := make(chan []byte)
 	receiveRequestErr := make(chan error)
-	go func(request chan string, receiveRequestErr chan error, consumer *kafka.Consumer) {
-		req, err := kafka_queue.ReceiveRequest(consumer)
+	go func(request chan []byte, receiveRequestErr chan error, consumer *kafka.Consumer) {
+		req, err := kafka_queue.ReceiveEvent(consumer)
 		request <- req
 		receiveRequestErr <- err
 	}(request, receiveRequestErr, consumer)
 	// Check the received request outputs
-	receivedRequest := <-request
+	receivedRequestBytes := <-request
 	receivedRequestErr := <-receiveRequestErr
 	if receivedRequestErr != nil {
-		return "", receivedRequestErr
+		return kafka_dto.KafkaIngressDTO{}, receivedRequestErr
+	}
+	var receivedRequest kafka_dto.KafkaIngressDTO
+	unmarshalErr := json.Unmarshal(receivedRequestBytes, &receivedRequest)
+	if unmarshalErr != nil {
+		return kafka_dto.KafkaIngressDTO{}, unmarshalErr
+
 	}
 	return receivedRequest, nil
 }
 
-func processSubmissionRequest(LEETCODE_SESSION string, CSRF_Token string, problem_slug string, problem_id int, lang string, code string) (string, string, error) {
-	for try := 0; try < 2; try++ {
-		submission_id, result, err := lib.Submit(LEETCODE_SESSION, CSRF_Token, problem_slug, problem_id, lang, code)
-		if err == nil && strings.Contains(result, "status_code") {
-			return submission_id, result, nil
-		}
-		if try == 0 {
-			lib.Login(leetcode_username, leetcode_password, "./leetcode-worker/drivers/chromedriver")
-		}
-	}
-	return "", "", fmt.Errorf("Failed to submit the solution")
+// TODO(minhhuy24072002): Integrate with raspbarry-pi workers
+func processSubmissionRequest(kafka_dto.KafkaIngressDTO) (kafka_dto.KafkaEgressDTO, error) {
+	return kafka_dto.KafkaEgressDTO{}, nil
 }
