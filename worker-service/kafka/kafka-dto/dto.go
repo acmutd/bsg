59f3eda935ef8702571183f9eadf97a906ef2760
minhhuy24072002 <minhhuy24072002@gmail.com>
2024-06-05 17:24:37 -0500

    add more detailed field for submission verdict

--- worker-service/kafka/kafka-dto/dto.go
+++ worker-service/kafka/kafka-dto/dto.go
@@ -2,6 +2,15 @@ package kafka
 
 import models "github.com/acmutd/bsg/worker-service/central-service-models"
 
+type SubmissionVerdict struct {
+	StatusMessage  string `json:"status_message"`
+	CodeOutput     string `json:"code_output"`
+	StdOutput      string `json:"std_output"`
+	ExpectedOutput string `json:"expected_output"`
+	LastTestcase   string `json:"last_testcase"`
+	ExecutionTime  string `json:"execution_time"`
+}
+
 type KafkaIngressDTO struct {
 	ProblemSlug  string `json:"problemSlug"`
 	ProblemId    uint   `json:"problemID"`
@@ -11,16 +20,13 @@ type KafkaIngressDTO struct {
 }
 
 type KafkaEgressDTO struct {
-	SubmissionId uint   `json:"submissionID"`
-	Verdict      string `json:"verdict"`
-
-	// this will store info such as which test case got wrong answer verdict, expected answer, etc..
-	Data []byte `json:"data"`
+	SubmissionId uint              `json:"submissionID"`
+	Verdict      SubmissionVerdict `json:"verdict"`
 }
 
 func NewKafkaIngressDTO(problem *models.Problem, submission *models.RoundSubmission) KafkaIngressDTO {
 	return KafkaIngressDTO{
-		ProblemSlug:  "", // TODO: replace with legit value,
+		ProblemSlug:  problem.Slug,
 		ProblemId:    problem.ID,
 		Lang:         submission.Submission.Language,
 		Code:         submission.Submission.Code,
