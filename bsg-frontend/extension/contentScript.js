515b539fa47f277d4773ac7ea787d56d2f507483
Yash Shah <62358049+git-yash@users.noreply.github.com>
2025-04-21 01:07:38 -0500

    removed unnecessary node dependencies for extension, removed border and padding on extension

--- bsg-frontend/extension/contentScript.js
+++ bsg-frontend/extension/contentScript.js
@@ -1,23 +1,20 @@
 ;(() => {
     const {host, pathname} = window.location;
     if (!host.includes("leetcode.com") || !pathname.startsWith("/problems/")) {
-        return;   // bail out unless we’re on a problem detail page
+        return;
     }
 
     const c = document.createElement("div");
     Object.assign(c.style, {
         position: "fixed",
-        bottom: "20px",
-        right: "20px",
+        bottom: "0px",
+        right: "0px",
         width: "360px",
         height: "600px",
         zIndex: "999999",
-        border: "1px solid #ccc",
-        background: "white",
     });
     document.body.appendChild(c);
 
-    // 2) an <iframe> pointing at your exported index.html
     const iframe = document.createElement("iframe");
     iframe.src = chrome.runtime.getURL("logIn.html");
     iframe.style = "width:100%; height:100%; border:none;";
