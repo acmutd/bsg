9193d2b278f67e2dc1b0b00e0b31da70a61a0bf7
Riyan Maknojia <riyanmak123@gmail.com>
2026-01-17 06:54:51 -0600

    changed to 1 hr

--- server/src/index.js
+++ server/src/index.js
@@ -21,7 +21,7 @@ app.use(session({
     resave: false,
     saveUninitialized: false,
     cookie: {
-        maxAge: 3600000, // 24 hours
+        maxAge: 3600000, // 1 hour
         httpOnly: true,
         secure: false, // Set to true in production with HTTPS
         sameSite: 'lax'
