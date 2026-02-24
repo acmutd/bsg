769e3d0d1359cb35209ccaa40caef8f9c983415c
Riyan Maknojia <riyanmak123@gmail.com>
2026-02-23 22:09:57 -0600

    refactored code for _app.tsx

--- bsg-frontend/apps/extension/pages/_app.tsx
+++ bsg-frontend/apps/extension/pages/_app.tsx
@@ -2,15 +2,26 @@ import '../../../packages/ui-styles/global.css'
 import type {AppProps} from 'next/app'
 import '@bsg/ui-styles/global.css';
 import {Poppins} from 'next/font/google'
+import DefaultPopup from './defaultPopup';
 
 const poppins = Poppins({weight: '400', subsets: ['latin']})
 
-export default function App({Component, pageProps}: AppProps) {
+export default function App({ Component, pageProps }: AppProps) {
 
-    return (
+    const isDefaultPopup = Component === DefaultPopup;
+    
+    if(isDefaultPopup){
         <div className={poppins.className}>
             <Component  {...pageProps} />
-        </div>
-    )
+        </div> 
+    }
+
+  return (
+    //TODO: Implement scrollbar code here
+    <div className={poppins.className}>
+      <Component  {...pageProps} />
+    </div>
+
+  )
 
 }
