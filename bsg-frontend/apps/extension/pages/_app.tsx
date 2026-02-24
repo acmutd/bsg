39ecf99ac3d7454f40ff3ef6cc9bf34db03e84f0
Riyan Maknojia <riyanmak123@gmail.com>
2026-02-23 22:09:57 -0600

    refactored code for _app.tsx

--- bsg-frontend/apps/extension/pages/_app.tsx
+++ bsg-frontend/apps/extension/pages/_app.tsx
@@ -9,13 +9,23 @@ import { faPaperPlane, faSmile, faCopy } from '@fortawesome/free-solid-svg-icons
 import { faGoogle } from '@fortawesome/free-brands-svg-icons'
 import RoomChoice from './room-choice'
 import { useChatSocket } from '../hooks/useChatSocket'
+import DefaultPopup from './defaultPopup';
 
 const poppins = Poppins({ weight: '400', subsets: ['latin'] })
 
 
 export default function App({ Component, pageProps }: AppProps) {
 
+    const isDefaultPopup = Component === DefaultPopup;
+    
+    if(isDefaultPopup){
+        <div className={poppins.className}>
+            <Component  {...pageProps} />
+        </div> 
+    }
+
   return (
+    //TODO: Implement scrollbar code here
     <div className={poppins.className}>
       <Component  {...pageProps} />
     </div>
