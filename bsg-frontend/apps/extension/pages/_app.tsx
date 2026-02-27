6e87c1cc133d67f4511006f3f4ba101bf6aeaac5
Calvin Tran <calvint05@outlook.com>
2026-02-27 16:35:01 -0600

    Fixed scrolling overflow and scrollbar theme

--- bsg-frontend/apps/extension/pages/_app.tsx
+++ bsg-frontend/apps/extension/pages/_app.tsx
@@ -1,27 +1,46 @@
-import '../../../packages/ui-styles/global.css'
-import type {AppProps} from 'next/app'
+import '../../../packages/ui-styles/global.css';
+import type { AppProps } from 'next/app';
 import '@bsg/ui-styles/global.css';
-import {Poppins} from 'next/font/google'
+import { Poppins } from 'next/font/google';
 import DefaultPopup from './defaultPopup';
+import { TabBar } from '@bsg/components/tabBar';
+import { Sidebar } from '@bsg/components/sidebar';
+import { useIsCollapsed } from '@/hooks/useIsCollapsed';
+import { useState } from 'react';
 
-const poppins = Poppins({weight: '400', subsets: ['latin']})
+const poppins = Poppins({ weight: '400', subsets: ['latin'] });
 
 export default function App({ Component, pageProps }: AppProps) {
 
-    const isDefaultPopup = Component === DefaultPopup;
-    
-    if(isDefaultPopup){
-        <div className={poppins.className}>
-            <Component  {...pageProps} />
-        </div> 
-    }
+  const isDefaultPopup = (Component === DefaultPopup);
+  const isCollapsed = useIsCollapsed();
+  const [isHovered, setIsHovered] = useState(false);
 
-  return (
-    //TODO: Implement scrollbar code here
-    <div className={poppins.className}>
-      <Component  {...pageProps} />
+  // Redirect popup render
+
+  if (isDefaultPopup) {
+    return (
+      <div className={poppins.className}>
+        <Component  {...pageProps} />
+      </div>
+    );
+  }
+
+  // On Leetcode extension render
+
+  return isCollapsed ? (
+    <Sidebar />
+  ) : (
+    //<div className={poppins.className}>
+    <div
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+      className="flex flex-col h-screen"
+    >
+      <TabBar isHovered={isHovered} isInRoom={true} />
+      <div className="flex-1 overflow-auto">
+        <Component {...pageProps} />
+      </div>
     </div>
-
-  )
-
-}
+  );
+};
