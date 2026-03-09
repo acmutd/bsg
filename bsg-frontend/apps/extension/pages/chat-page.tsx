34d17281e5952d5be8fa0649969e5891745d564d
Calvin Tran <calvint05@outlook.com>
2026-03-09 04:36:17 -0500

    Redesigned message input bar

--- bsg-frontend/apps/extension/pages/chat-page.tsx
+++ bsg-frontend/apps/extension/pages/chat-page.tsx
@@ -17,6 +17,7 @@ import LiveStatistics from "@bsg/components/liveStatistics/liveStatistics";
 import RoomChoice from "@/pages/room-choice";
 import { useRoomUser } from "@/hooks/useRoomUser";
 import { User } from "@bsg/models/User";
+import { Toolbar } from '@/customComponents/Toolbar/Toolbar'
 
 export default function ChatPage() {
     const {
@@ -40,36 +41,54 @@ export default function ChatPage() {
         setCurrentRoom
     } = useRoomUser();
 
-    // if (currentRoom) {
-    //const participants: User[] = currentRoom.options?.participants || []
-
     return (
-        <div ref={containerRef} className="flex-1 flex flex-col relative overflow-y-auto p-4 gap-4">
+        <div ref={containerRef} className="min-h-full flex-col relative p-4 gap-4">
             {messages.map((msg, i) => (
                 <div className={'bg-inputBackground'} key={i}>{msg.data}</div>
             ))}
-            <div className="absolute bottom-0 left-0  w-full px-4 py-4 flex items-center space-x-2">
-                <input
-                    ref={inputRef}
-                    className="flex-1 bg-[#333333] text-white rounded-full px-4 py-2"
-                    placeholder="Type a message..."
-                    onKeyDown={(e) => e.key === 'Enter' && sendMessage()}
-                />
-                <TooltipWrapper text={'Send Your Message'}>
-                    <Button onClick={sendMessage}
-                        className="bg-primary rounded-full w-10 h-10">
-                        <FontAwesomeIcon icon={faPaperPlane} />
-                    </Button>
-                </TooltipWrapper>
+
+            <div className="absolute bottom-0 left-0 w-full px-4 py-4 flex items-center">
+                <div className="flex w-full bg-[#333333] rounded-full items-center px-4 py-3 gap-4">
+                    <input
+                        ref={inputRef}
+                        className="outline-none bg-transparent text-foreground placeholder-foreground/60 w-full"
+                        placeholder="Type a message..."
+                        onKeyDown={(e) => e.key === 'Enter' && sendMessage()}
+                    />
+
+                    <TooltipWrapper text="Emojis">
+                        <Button
+                            //onClick={}
+                            className="rounded-full w-auto h-auto p-0 items-center justify-center bg-transparent hover:bg-transparent text-foreground/60 hover:text-foreground"
+                        >
+                            <svg
+                                className="w-5 h-5 overflow-visible"
+                                xmlns="http://www.w3.org/2000/svg"
+                                viewBox="0 0 36 36"
+                                fill="currentColor"
+                            >
+                                <path d="M32.625 18C32.625 9.92109 26.0789 3.375 18 3.375C9.92109 3.375 3.375 9.92109 3.375 18C3.375 26.0789 9.92109 32.625 18 32.625C26.0789 32.625 32.625 26.0789 32.625 18ZM0 18C0 8.05781 8.05781 0 18 0C27.9422 0 36 8.05781 36 18C36 27.9422 27.9422 36 18 36C8.05781 36 0 27.9422 0 18ZM12.4664 22.4578C13.5211 23.5547 15.3563 24.75 18 24.75C20.6437 24.75 22.4789 23.5547 23.5336 22.4578C24.1805 21.7828 25.2492 21.7617 25.9172 22.4086C26.5852 23.0555 26.6133 24.1242 25.9664 24.7922C24.4125 26.4094 21.7477 28.125 18 28.125C14.2523 28.125 11.5875 26.4094 10.0336 24.7922C9.38672 24.1172 9.40781 23.0484 10.0828 22.4086C10.7578 21.7688 11.8266 21.7828 12.4664 22.4578ZM10.125 14.625C10.125 13.3805 11.1305 12.375 12.375 12.375C13.6195 12.375 14.625 13.3805 14.625 14.625C14.625 15.8695 13.6195 16.875 12.375 16.875C11.1305 16.875 10.125 15.8695 10.125 14.625ZM23.625 12.375C24.8695 12.375 25.875 13.3805 25.875 14.625C25.875 15.8695 24.8695 16.875 23.625 16.875C22.3805 16.875 21.375 15.8695 21.375 14.625C21.375 13.3805 22.3805 12.375 23.625 12.375Z"/>
+                            </svg>
+                        </Button>
+                    </TooltipWrapper>
+
+                    <TooltipWrapper text="Send Message">
+                        <Button
+                            onClick={sendMessage}
+                            className="rounded-full w-auto h-auto p-0 items-center justify-center bg-transparent hover:bg-transparent text-foreground/60 hover:text-foreground"
+                        >
+                            <svg
+                                className="w-5 h-5 overflow-visible"
+                                xmlns="http://www.w3.org/2000/svg"
+                                viewBox="0 0 36 34"
+                                fill="currentColor"
+                            >
+                                <path d="M8.7048 18.6122L27.5602 18.6156L4.0054 29.8753L8.7048 18.6122ZM27.5557 15.3902L8.70503 15.3916L4.00725 4.12918L27.5557 15.3902ZM0.186475 3.25542L5.92143 17.0021L0.184499 30.7496C-0.181688 31.6237 0.0153813 32.6402 0.681867 33.3147C1.37182 34.0129 2.41855 34.1981 3.30573 33.7753L34.9813 18.6293C35.6056 18.33 35.9952 17.6982 36 16.9999C36.0047 16.3016 35.6058 15.6699 34.9815 15.3706L3.30344 0.224559C2.41633 -0.198145 1.36957 -0.0128098 0.679519 0.685522C0.0129362 1.3601 -0.18428 2.37666 0.181782 3.25067L0.186475 3.25542Z"/>
+                            </svg>
+                        </Button>
+                    </TooltipWrapper>
+                </div>
             </div>
         </div>
-    )
-    // }
-
-    return (
-        <RoomChoice
-            onJoin={handleJoin}
-            onCreate={handleCreate}
-        />
     )
 }
