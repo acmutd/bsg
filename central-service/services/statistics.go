7af9e85c7ca1e25d3af78099f90e1b812573eb01
Riyan Maknojia <riyanmak123@gmail.com>
2026-05-22 12:44:31 -0500

    user score function created

--- central-service/services/statistics.go
+++ central-service/services/statistics.go
@@ -1,13 +1,36 @@
 package services
 
 import (
+	"github.com/acmutd/bsg/central-service/models"
+	"github.com/redis/go-redis/v9"
 	"gorm.io/gorm"
 )
 
-type StatisticsService struct {
-	db *gorm.DB
+type Statistics struct {
+	db        *gorm.DB
+	rdb       *redis.Client
+	rtcClient *RTCClient
 }
 
-func InitializeStatisticService(db *gorm.DB) StatisticsService {
-	return StatisticsService{db}
+func InitializeStatisticService(db *gorm.DB, rdb *redis.Client, rtcClient *RTCClient) Statistics {
+	return Statistics{db, rdb, rtcClient}
+}
+
+type UserStatistics struct {
+	TotalScore uint `json:"score"`
+}
+
+// function to find the UserScore
+func (service *Statistics) GetUserScore(currentUser string, roomName string) (UserStatistics, error) {
+
+	var user models.Statistics
+	//first validate if user exist and is inside of room
+	result := service.db.Where("user_id = ?", currentUser).Where("room_id = ?", roomName).Find(&user)
+	if result.Error != nil {
+		return UserStatistics{}, result.Error
+	}
+
+	//user exist lets find their score
+	return UserStatistics{user.TotalScore}, nil
+
 }
