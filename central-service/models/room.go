4550d417f5ea1ed136376c5d325eaa40cd57aabe
Riyan Maknojia <riyanmak123@gmail.com>
2026-07-29 14:18:26 -0500

    roomMembers column created in db schema

--- central-service/models/room.go
+++ central-service/models/room.go
@@ -7,11 +7,12 @@ import (
 )
 
 type Room struct {
-	ID        uuid.UUID `gorm:"primaryKey" json:"id"`
-	ShortCode string    `gorm:"uniqueIndex;not null" json:"shortCode"`
-	Admin     string    `gorm:"not null" json:"adminId"`
-	Name      string    `json:"roomName"`
-	TTL       int       `json:"ttl"`       // TTL in minutes; 0 means no expiry
-	ExpiresAt *time.Time `json:"expiresAt"` // Nil means no expiry
-	Rounds    []Round   `gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;" json:"rounds"`
+	ID          uuid.UUID  `gorm:"primaryKey" json:"id"`
+	ShortCode   string     `gorm:"uniqueIndex;not null" json:"shortCode"`
+	Admin       string     `gorm:"not null" json:"adminId"`
+	Name        string     `json:"roomName"`
+	TTL         int        `json:"ttl"`       // TTL in minutes; 0 means no expiry
+	ExpiresAt   *time.Time `json:"expiresAt"` // Nil means no expiry
+	Rounds      []Round    `gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;" json:"rounds"`
+	RoomMembers []string   `gorm:"serializer:json; type:json" json:"roomMembers"`
 }
