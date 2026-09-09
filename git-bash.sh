07d1da9407be1763e6eb2a24531ae9563f309b5c
Riyan Maknojia <riyanmak123@gmail.com>
2026-09-09 11:04:24 -0500

    basic script set up

---
+++ git-bash.sh
@@ -0,0 +1,31 @@
+#/bin/bash
+if ! git rev-parse --is-inside-work-tree > /dev/null 2>&1; then
+	echo "Inside a working git tree" >&2
+else
+	echo "Failure not inside of git tree" >&2
+	exit 1
+fi
+
+if ! git diff --quiet || ! git diff --cached --quiet; then
+	echo "Error: commit or stash your changes first" >&2
+	exit 1
+fi
+
+git fetch --quiet
+
+if [ $? -eq 0 ]; then
+	echo "Git fetch worked" >&2
+else 
+	echo "Git fetch failed" >&2
+	exit 1
+fi
+
+
+
+
+
+
+
+
+
+
